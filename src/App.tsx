ed1f381e692d150da56a4dfbe7102d48f7822673
Beicker Zambrano <beicker998@gmail.com>
2022-03-10 17:39:23 -0500

    Create page title and some cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from "react";
+import { ChakraProvider, Flex, Box, Text } from "@chakra-ui/react";
+import theme from "./theme";
+import Card from "./components/Card";
+import IconButton from "./components/IconButton";
+import Icon from "./components/Icon";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+    <Box px="8" py="3" bg="bgPrimary" minHeight="100vh">
+      <Flex alignItems="center" gap="4" mb="4">
+        <Text fontSize="xl" fontWeight="bold">
+          Dashboard
+        </Text>
+        <Icon src="./icons/arrow-black.svg" alt="Arrow icon" />
+        <Flex
+          gap="3"
+          py="1"
+          px="3"
+          alignItems="center"
+          borderRadius="md"
+          bg="#fff"
+        >
+          <Text>Todas las tiendas</Text>
+
+          <Icon src="./icons/arrow-black.svg" alt="Arrow icon" />
+        </Flex>
+      </Flex>
+      <Flex justifyContent="space-between">
+        {[1, 2, 3].map((item) => {
+          return (
+            <React.Fragment key={item}>
+              <Card width="32%">
+                <Text>N° de productos en inventario</Text>
+                <Flex
+                  alignItems="center"
+                  width="100%"
+                  mt="2"
+                  justifyContent="space-between"
+                >
+                  <Text fontWeight="bold">1230</Text>
+                  <IconButton
+                    icon={
+                      <Icon src="./icons/arrow-white.svg" alt="Arrow Icon" />
+                    }
+                  >
+                    Ver detalles
+                  </IconButton>
+                </Flex>
+              </Card>
+            </React.Fragment>
+          );
+        })}
+      </Flex>
     </Box>
   </ChakraProvider>
-)
+);
