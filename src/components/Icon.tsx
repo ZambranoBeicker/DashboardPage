d33f97f9876d9850c216b9a5837b2de8965048a2
Beicker Zambrano <beicker998@gmail.com>
2022-03-14 23:47:36 -0500

    Update Icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,12 +4,21 @@ import { Image, Box } from "@chakra-ui/react";
 interface IconProps {
   src: string;
   alt: string;
+  size?: string;
+  rotate?: string;
 }
 
-export default function Icon({ src, alt }: IconProps) {
+export default function Icon({ src, alt, size, rotate }: IconProps) {
   return (
-    <Box>
-      <Image fit="contain" sizes="100%" src={src} alt={alt} />
+    <Box width={size} height={size}>
+      <Image
+        transform={rotate ? `rotateZ(${rotate})` : ""}
+        fit="contain"
+        height="100%"
+        width="100%"
+        src={src}
+        alt={alt}
+      />
     </Box>
   );
 }
