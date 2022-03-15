5340535cd0467097807d7595f8aa9beb1ec43af3
Beicker Zambrano <beicker998@gmail.com>
2022-03-14 23:47:50 -0500

    Update IconButton

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -5,16 +5,24 @@ import { Button } from "@chakra-ui/react";
 interface IconButtonProps {
   children: string;
   icon: any;
+  bgLight?: boolean;
+  margin?: string;
 }
 
-export default function IconButton({ children, icon }: IconButtonProps) {
+export default function IconButton({
+  children,
+  icon,
+  bgLight,
+  margin,
+}: IconButtonProps) {
   return (
     <Button
       fontWeight="light"
       display="flex"
       gap="3"
-      bg="blue.buttons"
-      color="#fff"
+      bg={bgLight ? "#fff" : "blue.buttons"}
+      color={bgLight ? "blue.buttons" : "#fff"}
+      margin={margin ? margin : "0"}
     >
       {children}
       {icon}
