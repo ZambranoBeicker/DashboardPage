4cde8df989084948c0161296761ff05722234ba3
Beicker Zambrano <beicker998@gmail.com>
2022-03-14 23:46:22 -0500

    Put all the necessary data in a file

---
+++ src/constants.ts
@@ -0,0 +1,100 @@
+export const barsChartInfo = [
+  { title: "Tasa de recompra", metric: "45%" },
+  { title: "Ticket promedio", metric: "Q. 1,223.55" },
+];
+export const barsChartData = [
+  {
+    id: "java",
+    label: "java",
+    value: 60,
+    color: "hsl(284, 70%, 50%)",
+  },
+  {
+    id: "javascript",
+    label: "javascript",
+    value: 20,
+    color: "hsl(322, 70%, 50%)",
+  },
+  {
+    id: "stylus",
+    label: "stylus",
+    value: 20,
+    color: "hsl(62, 70%, 50%)",
+  },
+];
+export const pieChartData = [
+  {
+    id: "java",
+    label: "java",
+    value: 60,
+    color: "hsl(284, 70%, 50%)",
+  },
+  {
+    id: "javascript",
+    label: "javascript",
+    value: 20,
+    color: "hsl(322, 70%, 50%)",
+  },
+  {
+    id: "stylus",
+    label: "stylus",
+    value: 20,
+    color: "hsl(62, 70%, 50%)",
+  },
+];
+export const lineChartData = [
+  {
+    id: "japan",
+    color: "hsl(1, 70%, 50%)",
+    data: [
+      {
+        x: "Ene",
+        y: 5,
+      },
+      {
+        x: "Feb",
+        y: 8,
+      },
+      {
+        x: "Mar",
+        y: 2,
+      },
+      {
+        x: "Abr",
+        y: 55,
+      },
+      {
+        x: "May",
+        y: 11,
+      },
+      {
+        x: "Jun",
+        y: 9,
+      },
+      {
+        x: "Jul",
+        y: 20,
+      },
+      {
+        x: "Ago",
+        y: 50,
+      },
+      {
+        x: "Sep",
+        y: 6,
+      },
+      {
+        x: "Oct",
+        y: 12,
+      },
+      {
+        x: "Nov",
+        y: 6,
+      },
+      {
+        x: "Dic",
+        y: 23,
+      },
+    ],
+  },
+];
