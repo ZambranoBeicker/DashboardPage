38562ae776bd4e99687dd5d166e404684bc90416
Beicker Zambrano <beicker998@gmail.com>
2022-03-14 23:47:20 -0500

    Update Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,20 +2,44 @@ import * as React from "react";
 import { Box } from "@chakra-ui/react";
 
 interface CardProps {
-  bgColor?: string;
   children: React.ReactNode;
   width?: string;
+  bgGradient?: boolean;
+  withoutPadding?: boolean;
 }
 
-export default function Card({ width, bgColor, children }: CardProps) {
+export default function Card({
+  width,
+  withoutPadding,
+  children,
+  bgGradient,
+}: CardProps) {
+  if (bgGradient) {
+    return (
+      <Box
+        borderRadius="md"
+        mb="4"
+        p={withoutPadding ? "0" : "4"}
+        shadow="5px 6px 15px #00000014"
+        width={width ? width : "auto"}
+        bgGradient="linear(to-r, #09BEE6, #1A2EAB)"
+        color="#fff"
+        overflow="hidden"
+      >
+        {children}
+      </Box>
+    );
+  }
+
   return (
     <Box
+      overflow="hidden"
       borderRadius="md"
       mb="4"
-      p="4"
-      bg={bgColor ? bgColor : "#fff"}
+      p={withoutPadding ? "0" : "4"}
       shadow="5px 6px 15px #00000014"
       width={width ? width : "auto"}
+      bg="#fff"
     >
       {children}
     </Box>
