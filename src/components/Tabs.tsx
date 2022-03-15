7905b4f250f8b5cabb1e5228f1a7b0985ccf6043
Beicker Zambrano <beicker998@gmail.com>
2022-03-14 23:41:14 -0500

    Create custom Tabs

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {
+  Tabs as ChakraTabs,
+  Tab,
+  TabPanels,
+  TabPanel,
+  TabList,
+} from "@chakra-ui/react";
+
+interface TabsProps {
+  valueList: string[];
+  children: React.ReactNode;
+}
+
+export default function Tabs({ valueList, children }: TabsProps) {
+  return (
+    <ChakraTabs
+      color="#828282"
+      borderBottomColor="border"
+      _selected={{ color: "blue.primary" }}
+      px="4"
+    >
+      <TabList>
+        {valueList.map((value, i) => (
+          <React.Fragment key={i}>
+            <Tab
+              px="10"
+              _selected={{ color: "blue.dark", borderColor: "blue.dark" }}
+            >
+              {value}
+            </Tab>
+          </React.Fragment>
+        ))}
+        {children}
+      </TabList>
+    </ChakraTabs>
+  );
+}
